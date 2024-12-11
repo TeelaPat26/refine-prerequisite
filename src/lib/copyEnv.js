813959f6d58fa26ad5bb0dc445937dd9791dae19
teelapatpra <teelapat.pra@gmail.com>
2024-12-11 12:14:37 +0700

    remove gently-copy

--- src/lib/copyEnv.js
+++
@@ -1,8 +0,0 @@
-const gentlyCopy = require('gently-copy');
-
-const source = '../env/.env';
-
-// User's local directory
-const destination = process.env.INIT_CWD
-
-gentlyCopy(source, destination, { overwrite: false })
